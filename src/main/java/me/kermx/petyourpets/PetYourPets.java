736b0cab086f902ad90b95e5f04f7dbc9cd73015
KermX <KermitVIII@gmail.com>
2023-04-14 00:16:10 -0500

    Initial commit

---
+++ src/main/java/me/kermx/petyourpets/PetYourPets.java
@@ -0,0 +1,17 @@
+package me.kermx.petyourpets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PetYourPets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
